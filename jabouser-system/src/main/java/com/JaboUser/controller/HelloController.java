6ce89395bcecf73354b947ca5cdbbf506ccc1307
恩嗯 <124288893@qq.com>
2024-05-23 01:13:53 +0800

    啦啦啦

---
+++ jabouser-system/src/main/java/com/JaboUser/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.JaboUser.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "朱静是傻逼";
+    }
+}
